7731cf8aa5f23e87c3ca43fade10deff9187355d
周卓 <yecan.xyc@alibaba-inc.com>
2025-01-26 17:08:02 +0800

    feat: 如果使用自定义dns解析，则耗时是独立的

--- EMASCurlTests/EMASCurlMetricObserverTest.m
+++ EMASCurlTests/EMASCurlMetricObserverTest.m
@@ -25,16 +25,14 @@ static NSURLSession *session;
 - (void)downloadDataWithMetrics:(NSString *)endpoint {
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", endpoint, PATH_DOWNLOAD_1MB_DATA_AT_200KBPS_SPEED]];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    
+
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-    
+
     __weak typeof(self) weakSelf = self;
 
     __block NSNumber *totalTimeConsumed = 0;
 
     [EMASCurlProtocol setMetricsObserverBlockForRequest:request metricsObserverBlock:^(NSURLRequest * _Nonnull request, double nameLookUpTimeMS, double connectTimeMs, double appConnectTimeMs, double preTransferTimeMs, double startTransferTimeMs, double totalTimeMs) {
-        typeof(self) strongSelf = weakSelf;
-
         XCTAssertGreaterThan(totalTimeMs, startTransferTimeMs, @"Total time should be after start transfer time");
 
         // Log metrics for debugging
@@ -50,24 +48,23 @@ static NSURLSession *session;
 
         totalTimeConsumed = [NSNumber numberWithDouble:totalTimeMs];
     }];
-    
+
     NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
                                                 completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         XCTAssertNil(error, @"Download failed with error: %@", error);
         XCTAssertNotNil(response, @"No response received");
-        
+
         NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
         XCTAssertEqual(httpResponse.statusCode, 200, @"Expected 200 status code");
         XCTAssertEqual(1024 * 1024, [data length], @"Expected 1MB of data");
-        
-        typeof(self) strongSelf = weakSelf;
+
         XCTAssertGreaterThan([totalTimeConsumed doubleValue], 0, @"Total time should be recorded");
-        
+
         dispatch_semaphore_signal(semaphore);
     }];
-    
+
     [dataTask resume];
-    
+
     XCTAssertEqual(dispatch_semaphore_wait(semaphore, dispatch_time(DISPATCH_TIME_NOW, 10 * NSEC_PER_SEC)), 0, @"Download request timed out");
 }
 
@@ -115,6 +112,81 @@ static NSURLSession *session;
 
 - (void)testDownloadDataWithMetrics {
     [self downloadDataWithMetrics:HTTP2_ENDPOINT];
+}
+
+@end
+
+@interface MockDNSResolver : NSObject <EMASCurlProtocolDNSResolver>
+@end
+
+@implementation MockDNSResolver
+
++ (NSString *)resolveDomain:(NSString *)domain {
+    // Simulate DNS resolution by sleeping for a known duration
+    [NSThread sleepForTimeInterval:2]; // 500ms delay
+    return @"127.0.0.1";
+}
+
+@end
+
+@interface EMASCurlMetricsTestCustomDNS : EMASCurlMetricsTestBase
+@end
+
+@implementation EMASCurlMetricsTestCustomDNS
+
+- (void)setUp {
+    [super setUp];
+    [EMASCurlProtocol setHTTPVersion:HTTP2];
+    [EMASCurlProtocol setDebugLogEnabled:YES];
+    [EMASCurlProtocol setDNSResolver:[MockDNSResolver class]];
+
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    [EMASCurlProtocol installIntoSessionConfiguration:config];
+    session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:nil];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testCustomDNSResolutionMetrics {
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", HTTP11_ENDPOINT, PATH_DOWNLOAD_1MB_DATA_AT_200KBPS_SPEED]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+
+    __block BOOL metricsReceived = NO;
+
+    double startTime = [[NSDate date] timeIntervalSince1970];
+
+    [EMASCurlProtocol setMetricsObserverBlockForRequest:request metricsObserverBlock:^(NSURLRequest * _Nonnull request, double nameLookUpTimeMS, double connectTimeMs, double appConnectTimeMs, double preTransferTimeMs, double startTransferTimeMs, double totalTimeMs) {
+
+        // Our mock resolver has a 500ms delay
+        XCTAssertGreaterThanOrEqual(nameLookUpTimeMS, 2000, @"DNS lookup time should be at least 2000ms with mock resolver");
+        XCTAssertLessThan(nameLookUpTimeMS, 2100, @"DNS lookup time should not be much more than 2100ms");
+
+        metricsReceived = YES;
+
+        // Log metrics for debugging
+        NSLog(@"Custom DNS Resolution Metrics:\n"
+              "DNS Lookup: %.2fms\n"
+              "Connect: %.2fms\n"
+              "Total: %.2fms",
+              nameLookUpTimeMS, connectTimeMs, totalTimeMs);
+    }];
+
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
+                                                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        XCTAssertTrue(metricsReceived, @"Metrics callback should have been received");
+        dispatch_semaphore_signal(semaphore);
+    }];
+
+    [dataTask resume];
+
+    // 确保解析dns的阻塞是发生在另一个线程
+    XCTAssertLessThan([[NSDate date] timeIntervalSince1970] - startTime, 0.1);
+
+    XCTAssertEqual(dispatch_semaphore_wait(semaphore, dispatch_time(DISPATCH_TIME_NOW, 10 * NSEC_PER_SEC)), 0, @"Request timed out");
 }
 
 @end
