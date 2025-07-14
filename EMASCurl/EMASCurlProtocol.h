34e2a1ccb6e930da6809fe8d629c87a1464b7845
周卓 <yecan.xyc@alibaba-inc.com>
2025-07-14 17:26:57 +0800

    chore: 发布1.3.3正式版

--- EMASCurl/EMASCurlProtocol.h
+++ EMASCurl/EMASCurlProtocol.h
@@ -10,7 +10,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define EMASCURL_SDK_VERSION @"1.3.2"
+#define EMASCURL_SDK_VERSION @"1.3.3"
 
 /// 提供一个便捷易用的DNS Hook机制，类似OKHTTP中的DNS配置
 @protocol EMASCurlProtocolDNSResolver <NSObject>
